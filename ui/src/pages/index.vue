4c039393ecbd1a534635ac55d4c69c26a96f13b9
Marcin <czyzmarcin1@gmail.com>
2024-02-05 20:46:59 +0100

    update

--- ui/src/pages/index.vue
+++ ui/src/pages/index.vue
@@ -3,44 +3,17 @@ defineOptions({
   name: 'IndexPage',
 })
 
-const name = ref('')
+// const name = ref('')
 
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+// const router = useRouter()
+
+const hi = () => {
+  console.log('hi')
 }
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div>
+  <div @click="hi()">
+   hi
   </div>
 </template>
